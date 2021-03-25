388da304d9eef2a7c0ca140b09523cb030bdd6de
kangaroo-demo <932376816@qq.com>
2021-03-25 15:33:37 +0800

    add update user info

--- src/main.js
+++ src/main.js
@@ -8,10 +8,12 @@ import './components'
 // 引入 store
 import store from './store'
 // 引入插件
-import VueSweetalert2 from './plugins/vue-sweetalert2'    
+import VueSweetalert2 from './plugins/vue-sweetalert2'
+import Message from './plugins/message'  
 
 // 使用插件
 Vue.use(VueSweetalert2)
+Vue.use(Message)
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -7,18 +7,26 @@ Vue.use(Router)
 
 const router = new Router({
     mode: 'history',
+    linkExactActiveClass: 'active',
     routes
 })
 
 // 全局前置守卫
 router.beforeEach((to, from, next) => {
-    const auth = router.app.$options.store.state.auth
-
-    if (auth && to.path.indexOf('/auth/') !== -1) {
-        next('/')
+    const app = router.app
+    const store = app.$options.store
+    const auth = store.state.auth
+  
+    app.$message.hide()
+  
+    if (
+      (auth && to.path.indexOf('/auth/') !== -1) ||
+      (!auth && to.meta.auth)
+    ) {
+      next('/')
     } else {
-        next()
+      next()
     }
-})
+  })
   
 export default router
